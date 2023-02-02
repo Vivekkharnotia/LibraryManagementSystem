0375622982ef9f12604f69805f4ba5083d21b27a
alirmemon37 <alirmemona@gmail.com>
2023-02-02 22:34:13 +0530

    hero and footer added

---
+++ components/general/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import MobileNavbar from "./MobileNavbar"
+import useDevice from "../../utils/useDevice";
+
+const Navbar = () => {
+  const { isMobile } = useDevice();
+
+  return (
+    <>
+      {!isMobile ? (
+        <div className="flex flex-row items-center px-16 py-8 justify-end">
+          <ul className="flex flex-row gap-8">
+            <li>Blogs</li>
+            <li>Slots</li>
+            <li>Contact</li>
+            <li>About</li>
+            <li>Sign in</li>
+            <li>Log in</li>
+          </ul>
+        </div>
+      ) : (
+        <>
+          <MobileNavbar />
+        </>
+      )}
+    </>
+  );
+};
+
+export default Navbar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
+import Hero from "../../components/general/Hero"
+import Footer from "../../components/general/Footer"
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline py-4">Hello world!</h1>
+      <Hero />
+      <Footer />
     </>
   );
 }
