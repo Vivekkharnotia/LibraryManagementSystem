de2e1dce7960ffb3528191474fe2bf46de7c2a3b
alirmemon37 <alirmemona@gmail.com>
2023-02-02 22:55:55 +0530

    refactor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,11 +4,15 @@ import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import Hero from "../../components/general/Hero"
 import Footer from "../../components/general/Footer"
+import SignIn from "../../components/general/SignIn/SignIn"
+import Services from "../../components/general/Services/Services"
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <SignIn />
+      <Services />
       <Footer />
     </>
   );
