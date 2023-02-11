d6c84b06b5e2ed1c1054fea34bbb09d3176749bd
alirmemon37 <alirmemona@gmail.com>
2023-02-11 23:10:02 +0530

    services seciton refactor

--- components/general/Services/Services.js
+++ components/general/Services/Services.js
@@ -1,47 +1,50 @@
 import services from "./Services.module.css";
 import Service from "./Service";
 
-import secured from '../../public/secure.png';
-import online_consultancy from '../../public/online_consultancy.png';
-import easy_to_use from '../../public/easy.png';
-import experts from '../../public/experts.png';
-import video_illustrations from '../../public/video.png';
-import blogs from '../../public/blogs.png';
+// import secured from "../../public/secure.png";
+// import online_consultancy from "../../public/online_consultancy.png";
+// import easy_to_use from "../../public/easy.png";
+// import experts from "../../public/experts.png";
+// import video_illustrations from "../../public/video.png";
+// import blogs from "../../public/blogs.png";
 
 function Services() {
-    
-    return(
-  <>
-    <div className={services.title}>Services</div>
-    <div className={services.container}>
+  return (
+    <>
+      <div className={services.title}>Services</div>
+      <div className={services.container}>
         <div className={services.row1}>
-            <Service image={secured} name="Secured"/>
-            <Service image={online_consultancy} name="Online Consultancy"/>
-            <Service image={video_illustrations} name="Video Illustrations"/>
+          <Service image="/secure.png" name="Secured" />
+          <Service
+            image="/online_consultancy.png"
+            name="Online Consultancy"
+          />
+          <Service image="/video.png" name="Video Illustrations" />
         </div>
         <div className={services.row2}>
-            <Service image={blogs} name="Blogs"/>
-            <Service image={experts} name="Experts"/>
-            <Service image={easy_to_use} name="Easy to use"/>
+          <Service image="/blogs.png" name="Blogs" />
+          <Service image="/experts.png" name="Experts" />
+          <Service image="/easy.png" name="Easy to use" />
         </div>
-
 
         <div className={services.row3}>
-            <Service image={secured} name="Secured"/>
-            <Service image={online_consultancy} name="Online Consultancy"/>
+          <Service image="/secure.png" name="Secured" />
+          <Service
+            image="/online_consultancy.png"
+            name="Online Consultancy"
+          />
         </div>
         <div className={services.row4}>
-            <Service image={video_illustrations} name="Video Illustrations"/>
-            <Service image={blogs} name="Blogs"/>
+          <Service image="/video.png" name="Video Illustrations" />
+          <Service image="/blogs.png" name="Blogs" />
         </div>
         <div className={services.row5}>
-            <Service image={experts} name="Experts"/>
-            <Service image={easy_to_use} name="Easy to use"/>
+          <Service image="/experts.png" name="Experts" />
+          <Service image="/easy.png" name="Easy to use" />
         </div>
-
-        
-    </div>
-  </>);
+      </div>
+    </>
+  );
 }
 
 export default Services;
