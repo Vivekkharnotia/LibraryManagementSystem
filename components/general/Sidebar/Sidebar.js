a6fbf5c25d0bb1247f5cfe1ff857602c3d5781bc
alirmemon37 <alirmemona@gmail.com>
2023-02-14 20:35:20 +0530

    Appoinments section added in Sidebar mobile view

--- components/general/Sidebar/Sidebar.js
+++ components/general/Sidebar/Sidebar.js
@@ -9,7 +9,7 @@ import List from "@mui/material/List";
 import CssBaseline from "@mui/material/CssBaseline";
 import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
-import ArrowForwardOutlinedIcon from '@mui/icons-material/ArrowForwardOutlined';
+import ArrowForwardOutlinedIcon from "@mui/icons-material/ArrowForwardOutlined";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
@@ -20,7 +20,7 @@ import PhoneIcon from "@mui/icons-material/Phone"; //call
 import MenuIcon from "@mui/icons-material/Menu"; // menu
 import sidebar from "./Sidebar.module.css";
 import { Button, Typography } from "@mui/material";
-import Appointments from "../Appointments"
+import Appointments from "../Appointments";
 
 const drawerWidth = 240;
 
@@ -96,16 +96,16 @@ export default function MiniDrawer() {
   const [current2, setCurrent2] = useState(2);
 
   const handleDrawerOpen = () => {
-    let drawerBtn = document.getElementById('drawerBtn');
-    drawerBtn.style.rotate = '180deg';
-    drawerBtn.style.transition = '0.5s ease';
+    let drawerBtn = document.getElementById("drawerBtn");
+    drawerBtn.style.rotate = "180deg";
+    drawerBtn.style.transition = "0.5s ease";
     setOpen(true);
   };
 
   const handleDrawerClose = () => {
-    let drawerBtn = document.getElementById('drawerBtn');
-    drawerBtn.style.rotate = '0deg';
-    drawerBtn.style.transition = '0.5s ease';
+    let drawerBtn = document.getElementById("drawerBtn");
+    drawerBtn.style.rotate = "0deg";
+    drawerBtn.style.transition = "0.5s ease";
     setOpen(false);
   };
 
@@ -126,167 +126,206 @@ export default function MiniDrawer() {
   };
 
   const view2activeElement = (ind) => {
-    let view2Bar = document.getElementById('view2Bar');
-    view2Bar.style.left = `${ind*20}vw`;
-    document.getElementById(`view2Item${current2}`).style.color = 'black';
+    let view2Bar = document.getElementById("view2Bar");
+    view2Bar.style.left = `${ind * 20}vw`;
+    document.getElementById(`view2Item${current2}`).style.color = "black";
     setCurrent2(ind);
-    document.getElementById(`view2Item${ind}`).style.color = 'white';
-  }
+    document.getElementById(`view2Item${ind}`).style.color = "white";
+  };
 
   return (
     <>
-    <Box sx={{ display: "flex" }} className={sidebar.view1}>
-      <CssBaseline />
-      <AppBar position="fixed" open={open} sx={{ backgroundColor: "white" }} elevation={1}>
-        <Toolbar>
-          
-          <Typography sx={{ color: "black" }}>Navbar content</Typography>
-          {/*--------------------------------------Navbar content -------------------------------------  */}
-        </Toolbar>
-      </AppBar>
+      <Box sx={{ display: "flex" }} className={sidebar.view1}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          open={open}
+          sx={{ backgroundColor: "white" }}
+          elevation={1}
+        >
+          <Toolbar>
+            <Typography sx={{ color: "black" }}>Navbar content</Typography>
+            {/*--------------------------------------Navbar content -------------------------------------  */}
+          </Toolbar>
+        </AppBar>
 
-      <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
-          {/* <IconButton onClick={handleDrawerClose}>
+        <Drawer variant="permanent" open={open}>
+          <DrawerHeader>
+            {/* <IconButton onClick={handleDrawerClose}>
             <ChevronLeftIcon />
           </IconButton> */}
-          <Typography className={sidebar.drawerHead} variant="h5">Reh-A</Typography>
-        </DrawerHeader>
-        <Divider />
+            <Typography className={sidebar.drawerHead} variant="h5">
+              Reh-A
+            </Typography>
+          </DrawerHeader>
+          <Divider />
 
-        <List>
-          {/* <div className={sidebar.sidebarBtn}> */}
-          <ListItemButton
-            sx={{
-              height: "auto",
-              justifyContent: open ? "initial" : "center",
-              px: 2.5,
-              paddingX: "20px",
-              paddingY: "25px",
-            }}
-            className = {sidebar.sidebarBtn}
-            onClick={!open ? handleDrawerOpen : handleDrawerClose}
-          >
-            <ListItemIcon
+          <List>
+            {/* <div className={sidebar.sidebarBtn}> */}
+            <ListItemButton
               sx={{
-                minWidth: 0,
-                mr: open ? 3 : "auto",
-                justifyContent: "center",
+                height: "auto",
+                justifyContent: open ? "initial" : "center",
+                px: 2.5,
+                paddingX: "20px",
+                paddingY: "25px",
               }}
-              className = {sidebar.sidebarIcn}
-              id='drawerBtn'
+              className={sidebar.sidebarBtn}
+              onClick={!open ? handleDrawerOpen : handleDrawerClose}
             >
-              <ArrowForwardOutlinedIcon fontSize="medium" color="action"/>
-            </ListItemIcon>
+              <ListItemIcon
+                sx={{
+                  minWidth: 0,
+                  mr: open ? 3 : "auto",
+                  justifyContent: "center",
+                }}
+                className={sidebar.sidebarIcn}
+                id="drawerBtn"
+              >
+                <ArrowForwardOutlinedIcon fontSize="medium" color="action" />
+              </ListItemIcon>
             </ListItemButton>
 
-          <div className={sidebar.bar} id="bar"></div>
+            <div className={sidebar.bar} id="bar"></div>
 
-          {["Blogs", "My appoinments", "Talk with us", "Profile", "More"].map(
-            (text, index) => (
-              <ListItem
-                key={text}
-                disablePadding
-                sx={{ display: "block", color: index == 0 ? "white" : "black" }}
-                id={`item${index}`}
-                className={sidebar.elements}
-              >
-                <ListItemButton
+            {["Blogs", "My appoinments", "Talk with us", "Profile", "More"].map(
+              (text, index) => (
+                <ListItem
+                  key={text}
+                  disablePadding
                   sx={{
-                    height: "auto",
-                    justifyContent: open ? "initial" : "center",
-                    px: 2.5,
-                    paddingX: "20px",
-                    paddingY: "25px",
+                    display: "block",
+                    color: index == 0 ? "white" : "black",
                   }}
-                  onClick={() => activeElement(index)}
+                  id={`item${index}`}
+                  className={sidebar.elements}
                 >
-                  <ListItemIcon
+                  <ListItemButton
                     sx={{
-                      minWidth: 0,
-                      mr: open ? 3 : "auto",
-                      justifyContent: "center",
+                      height: "auto",
+                      justifyContent: open ? "initial" : "center",
+                      px: 2.5,
+                      paddingX: "20px",
+                      paddingY: "25px",
                     }}
+                    onClick={() => activeElement(index)}
                   >
-                    {index == 0 ? (
-                      <ImportContactsIcon
-                        id={`icon${index}`}
-                        sx={{ color: "white" }}
-                      />
-                    ) : null}
-                    {index == 1 ? (
-                      <CalendarMonthIcon
-                        id={`icon${index}`}
-                        sx={{ color: "black" }}
-                      />
-                    ) : null}
-                    {index == 2 ? (
-                      <PhoneIcon id={`icon${index}`} sx={{ color: "black" }} />
-                    ) : null}
-                    {index == 3 ? (
-                      <PermIdentityIcon
-                        id={`icon${index}`}
-                        sx={{ color: "black" }}
-                      />
-                    ) : null}
-                    {index == 4 ? (
-                      <MenuIcon id={`icon${index}`} sx={{ color: "black" }} />
-                    ) : null}
-                  </ListItemIcon>
+                    <ListItemIcon
+                      sx={{
+                        minWidth: 0,
+                        mr: open ? 3 : "auto",
+                        justifyContent: "center",
+                      }}
+                    >
+                      {index == 0 ? (
+                        <ImportContactsIcon
+                          id={`icon${index}`}
+                          sx={{ color: "white" }}
+                        />
+                      ) : null}
+                      {index == 1 ? (
+                        <CalendarMonthIcon
+                          id={`icon${index}`}
+                          sx={{ color: "black" }}
+                        />
+                      ) : null}
+                      {index == 2 ? (
+                        <PhoneIcon
+                          id={`icon${index}`}
+                          sx={{ color: "black" }}
+                        />
+                      ) : null}
+                      {index == 3 ? (
+                        <PermIdentityIcon
+                          id={`icon${index}`}
+                          sx={{ color: "black" }}
+                        />
+                      ) : null}
+                      {index == 4 ? (
+                        <MenuIcon id={`icon${index}`} sx={{ color: "black" }} />
+                      ) : null}
+                    </ListItemIcon>
 
-                  <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-                </ListItemButton>
-              </ListItem>
-            )
-          )}
-        </List>
-        <Divider />
-      </Drawer>
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <DrawerHeader />
+                    <ListItemText
+                      primary={text}
+                      sx={{ opacity: open ? 1 : 0 }}
+                    />
+                  </ListItemButton>
+                </ListItem>
+              )
+            )}
+          </List>
+          <Divider />
+        </Drawer>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <DrawerHeader />
 
-        {/* --------------------------------- Elements outside the sidebar and navbar-------------------------- */}
-        {/* <Typography>
+          {/* --------------------------------- Elements outside the sidebar and navbar-------------------------- */}
+          {/* <Typography>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, error
           unde recusandae quia voluptate maxime consectetur? Tenetur fugit
           cumque nesciunt!
         </Typography> */}
 
-        <Appointments />
-        
+          <Appointments />
+        </Box>
       </Box>
-    </Box>
 
-    {/* media query 768px (tablet view)  */}
-    <div className={sidebar.view2}>
+      {/* media query 768px (tablet view)  */}
+      <div className={sidebar.view2}>
+        {/* ---------------------------------------------- Content of the bage ----------------------- */}
+        <Appointments />
 
-      {/* ---------------------------------------------- Content of the bage ----------------------- */}
-
-      <div className={sidebar.bottomNav}>
-          <div className={sidebar.view2Bar} id = 'view2Bar'></div>
-          <Button className={sidebar.view2Item} onClick = {() => view2activeElement(0)}>
-            <ImportContactsIcon id = {`view2Item${0}`} className={sidebar.view2Icn}/>
+        <div className={sidebar.bottomNav}>
+          <div className={sidebar.view2Bar} id="view2Bar"></div>
+          <Button
+            className={sidebar.view2Item}
+            onClick={() => view2activeElement(0)}
+          >
+            <ImportContactsIcon
+              id={`view2Item${0}`}
+              className={sidebar.view2Icn}
+            />
           </Button>
-          <Button className={sidebar.view2Item} onClick = {() => view2activeElement(1)}>
-            <PhoneIcon id = {`view2Item${1}`} className={sidebar.view2Icn}/>
+          <Button
+            className={sidebar.view2Item}
+            onClick={() => view2activeElement(1)}
+          >
+            <PhoneIcon id={`view2Item${1}`} className={sidebar.view2Icn} />
           </Button>
-          <Button className={sidebar.view2Item} onClick = {() => view2activeElement(2)}>
-            <CalendarMonthIcon id = {`view2Item${2}`} sx={{color : 'white'}} className={sidebar.view2Icn}/>
+          <Button
+            className={sidebar.view2Item}
+            onClick={() => view2activeElement(2)}
+          >
+            <CalendarMonthIcon
+              id={`view2Item${2}`}
+              sx={{ color: "white" }}
+              className={sidebar.view2Icn}
+            />
           </Button>
-          <Button className={sidebar.view2Item} onClick = {() => view2activeElement(3)}>
-            <PermIdentityIcon id = {`view2Item${3}`} className={sidebar.view2Icn}/>
+          <Button
+            className={sidebar.view2Item}
+            onClick={() => view2activeElement(3)}
+          >
+            <PermIdentityIcon
+              id={`view2Item${3}`}
+              className={sidebar.view2Icn}
+            />
           </Button>
-          <Button className={sidebar.view2Item} onClick = {() => view2activeElement(4)}>
-            <MenuIcon id = {`view2Item${4}`} className={sidebar.view2Icn}/>
+          <Button
+            className={sidebar.view2Item}
+            onClick={() => view2activeElement(4)}
+          >
+            <MenuIcon id={`view2Item${4}`} className={sidebar.view2Icn} />
           </Button>
+        </div>
       </div>
-      
-    </div>
     </>
   );
 }
 
-{/* <IconButton
+{
+  /* <IconButton
             id = 'drawerBtn'
             color="inherit"
             aria-label="open drawer"
@@ -298,4 +337,5 @@ export default function MiniDrawer() {
             }}
           >
             <ArrowRightAltIcon fontSize="large" sx={{ color: "black" }} />
-          </IconButton> */}
+          </IconButton> */
+}
