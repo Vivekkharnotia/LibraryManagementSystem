15ac9e61adbeca3aec6e5fe37e0a3cd8cc14d32d
Anas-025 <200280107097@Ldceahm.gujgov.edu.in>
2023-02-14 21:19:25 +0530

    my changes

--- components/general/Hero.jsx
+++ components/general/Hero.jsx
@@ -21,13 +21,13 @@ const Hero = () => {
       <div
         className={`relative h-[650px] max-w-[1440px] m-auto hero-break:bg-[#262525]`}
       >
-          <div>
-            <img
+        <div>
+          <img
               src={`/bg-hero-${activeImage}.png`}
               alt="Hero background"
               className="absolute z-0 object-cover object-right w-[100%] h-[650px] hero-break:hidden"
             />
-          </div>
+        </div>
         <div className="flex flex-col h-full z-20 relative">
           <Navbar />
           <div className="flex flex-col gap-8 h-full px-8 md:px-16 justify-center md:-mt-16">
