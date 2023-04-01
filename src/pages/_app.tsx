f4998016be6e08e3b9eb48b99b4b3c4daaa6178f
alirmemon37 <alirmemona@gmail.com>
2023-04-02 01:55:24 +0530

    Meeting Context added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/router";
 import Layout from "./Layout";
 import { UserProvider } from "components/UserContext";
 import { Suspense } from "react";
+import { MeetingProvider } from "components/MeetingContext";
 
 const Loading = () => <div className="bg-[#000]">Loading...</div>;
 
@@ -16,26 +17,34 @@ export default function App({ Component, pageProps }: AppProps) {
     return (
       <Suspense fallback={<Loading />}>
         <UserProvider>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
+          <MeetingProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </MeetingProvider>
+        </UserProvider>
+      </Suspense>
+    );
+  } else if (router.pathname.startsWith("/blogs")) {
+    return (
+      <Suspense fallback={<Loading />}>
+        <UserProvider>
+          <MeetingProvider>
+            <BlogsLayout>
+              <Component {...pageProps} />
+            </BlogsLayout>
+          </MeetingProvider>
         </UserProvider>
       </Suspense>
     );
   }
 
-  else if(router.pathname.startsWith('/blogs')) {
-    return (
-      <BlogsLayout>
-        <Component {...pageProps} />
-      </BlogsLayout>
-    )
-  }
-
   return (
     <Suspense fallback={<Loading />}>
       <UserProvider>
-        <Component {...pageProps} />
+        <MeetingProvider>
+          <Component {...pageProps} />
+        </MeetingProvider>
       </UserProvider>
     </Suspense>
   );
