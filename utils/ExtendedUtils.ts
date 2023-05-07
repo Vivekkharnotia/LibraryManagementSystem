eec00e14e13db3fc35db2851758b328fb6acc0b6
alirmemon37 <alirmemona@gmail.com>
2023-05-07 13:38:18 +0530

    Profile Image functionality added

--- utils/ExtendedUtils.ts
+++ utils/ExtendedUtils.ts
@@ -1,3 +1,6 @@
+import { fbStorage } from "components/general/firebase-config";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+
 export function returnFileSize(number: number) {
   if (number < 1024) {
     return `${number} bytes`;
@@ -13,3 +16,30 @@ const fileTypes = ["image/jpeg", "image/png"];
 export function validFileType(file: File): boolean{
   return fileTypes.includes(file.type);
 }
+
+export const uploadFileToFirebaseAndGetUrl = async (file: File | null) => {
+  if (!file) {
+    return { uploadedToUrl: "", path: "" };
+  }
+
+  const newFile = new File([file], file?.name.replace(/\s/g, "_"), {
+    type: file?.type,
+  });
+  let type = newFile.type.split("/")[0];
+  if (!type) {
+    type = "other";
+  }
+  const path = `UserProfiles/${type}/${newFile.name}`;
+  const storageRef = ref(fbStorage, path);
+
+  const uploadedToUrl = await uploadBytes(storageRef, newFile).then(
+    async (snapshot) => {
+      //return newFile url
+      const url = await getDownloadURL(snapshot.ref).then((url) => {
+        return url;
+      });
+      return url;
+    }
+  );
+  return { uploadedToUrl, path };
+};
