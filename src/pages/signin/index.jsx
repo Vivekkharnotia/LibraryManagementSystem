e8111b59383a9ba417ab4014f7636397e07eb026
alirmemon37 <alirmemona@gmail.com>
2023-05-30 00:57:46 +0530

    SignIn page added

---
+++ src/pages/signin/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignIn from "../../../components/general/SignIn/SignIn"
+
+const index = () => {
+  return (
+    <>
+      <SignIn />
+    </>
+  )
+}
+
+export default index
