c3a73c5428815b0bbf0b9721fecdf486c535b62f
alirmemon37 <alirmemona@gmail.com>
2023-06-16 00:43:12 +0530

    (update): blog cover image

--- components/general/BlogComponents/HeroImage/HeroImage.tsx
+++ components/general/BlogComponents/HeroImage/HeroImage.tsx
@@ -1,60 +1,92 @@
 import EditIcon from "@mui/icons-material/Edit";
+import AddPhotoAlternateOutlinedIcon from "@mui/icons-material/AddPhotoAlternateOutlined";
 import { IconButton, Tooltip, Zoom } from "@mui/material";
-import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
+import { Dispatch, SetStateAction, useRef, useState } from "react";
 import { returnFileSize, validFileType } from "utils/ExtendedUtils";
 import style from "../../BlogCreator/VisitBlog.module.css";
+import Image from "next/image";
 
-
-export default function HeroImage({ heroImageSrc, setHeroImageSrc }: { heroImageSrc: string, setHeroImageSrc: Dispatch<SetStateAction<string>>}) {
+export default function HeroImage({
+  blogCoverImage,
+  setBlogCoverImage,
+  blogCoverImageFile,
+  setBlogCoverImageFile,
+}: {
+  blogCoverImage: string;
+  setBlogCoverImage: Dispatch<SetStateAction<string>>;
+  blogCoverImageFile: File | null;
+  setBlogCoverImageFile: Dispatch<SetStateAction<File | null>>;
+}) {
   const [tooltipTitle, setTooltipTitle] = useState("Edit Image");
-  const heroImageRef = useRef<HTMLInputElement>(null);
+  const blogCoverImageRef = useRef<HTMLInputElement>(null);
 
+  const handleBlogCoverImageChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (!e?.target?.files) return;
 
-  useEffect(() => {
-    if (heroImageRef.current) {
-        heroImageRef.current.addEventListener("change", (e) => {
-        // @ts-ignore
-        const file = e.target?.files[0];
+    const file = e.target?.files[0];
 
-        if (file) {
-            if(!validFileType(file)){ 
-                alert("Invalid File Type");
-            }else{
-            const src = URL.createObjectURL(file);
-            setTooltipTitle(`${file.name} (${returnFileSize(file.size)})`)
-            setHeroImageSrc(src);
-            }
-        }
-        });
+    if (file) {
+      if (!validFileType(file)) {
+        alert("Invalid File Type");
+      } else {
+        const src = URL.createObjectURL(file);
+        setTooltipTitle(`${file.name} (${returnFileSize(file.size)})`);
+        setBlogCoverImage(src);
+        setBlogCoverImageFile(file);
+      }
     }
-    }, [heroImageRef]);
-
-
+  };
 
   return (
-    <div className={style.image1} id="heroImage">
-      <div>
+    <div className="relative mt-[70px] w-full text-center" id="blogCoverImage">
+      <div className="relative m-auto overflow-auto max-w-[900px] h-[600px] px-4">
         <Tooltip title={tooltipTitle} TransitionComponent={Zoom}>
-          <img
-            src={heroImageSrc}
-            alt=""
-          />
+          <div className="w-full h-full bg-gray-200 rounded-[10px] overflow-hidden cursor-pointer">
+            {blogCoverImageFile ? (
+              <img
+                src={URL.createObjectURL(blogCoverImageFile)}
+                className="w-full h-full object-cover"
+                alt=""
+              />
+            ) : blogCoverImage ? (
+              <Image
+                src={blogCoverImage}
+                fill={true}
+                alt="Blog Cover Image"
+                className="object-cover cursor-pointer rounded-[10px] transition-all duration-150"
+                onClick={() => blogCoverImageRef.current?.click()}
+              />
+            ) : (
+              <div
+                onClick={() => blogCoverImageRef.current?.click()}
+                className="flex flex-col gap-4 items-center justify-center w-full h-full"
+              >
+                <AddPhotoAlternateOutlinedIcon className="text-white text-6xl" />
+                <span className="text-xl md:text-2xl font-semibold text-white">
+                  Upload Blog Cover Image
+                </span>
+              </div>
+            )}
+          </div>
         </Tooltip>
-        <IconButton className={style.editIcon}>
-          <label htmlFor="file" className={style.addImage}>
+        <IconButton className="absolute right-6 top-2 z-40 w-12 h-12 cursor-pointer bg-black flex items-center justify-center hover:bg-gray-400">
+          <label htmlFor="file" className="grid items-start cursor-pointer">
             <input
-              ref={heroImageRef}
+              ref={blogCoverImageRef}
               type="file"
               name=""
               id="file"
               className={style.file}
               accept=".jpg, .jpeg, .png"
+              hidden
+              onChange={handleBlogCoverImageChange}
             />
-            <EditIcon style={{ color: "white"}} />
+            <EditIcon style={{ color: "white" }} />
           </label>
         </IconButton>
       </div>
     </div>
   );
 }
-
